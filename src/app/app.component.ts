ef01bc41c5a35fd25fb1205f613a6fd5aa4e43ae
Davia123 <alexdavia2003@gmail.com>
2024-01-17 16:46:08 +0100

    💄 update layout | show - hide header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'GameVerse';
+  mostrarHeaderFooter = true;
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log(event);
+        if (event.url === '/') {
+          this.mostrarHeaderFooter = false;
+        } else {
+          this.mostrarHeaderFooter = true;
+        }
+        console.log(this.mostrarHeaderFooter);
+      }
+    });
+    console.log(this.router.url);
+  }
 }
